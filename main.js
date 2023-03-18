066235a66562b583eb1bbf2f61e6b0c92c023989
kkj2010 <kunjukim0210@gmail.com>
2023-03-18 13:50:00 -0400

    add timer

--- main.js
+++ main.js
@@ -1,36 +1,98 @@
-'use strict'
+"use strict";
 
-const CARROT_SIZE= 80;
-const field= document.querySelector('.game_field')
-const fieldRect= field.getBoundingClientRect();
+const CARROT_SIZE = 80;
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5;
+const GAME_DURATION_SEC = 5;
 
-function initGame(){
-  console.log(fieldRect);
-  addItem('carrot', 5, 'img/carrot.png');
-  addItem('bug', 5, 'img/bug.png')
+const field = document.querySelector(".game_field");
+const fieldRect = field.getBoundingClientRect();
+const gameBtn = document.querySelector(".game_button");
+const gameTimer = document.querySelector(".game_timer");
+const gameScore = document.querySelector(".game_score");
+
+let started = false;
+let score = 0;
+let timer = undefined;
+
+gameBtn.addEventListener("click", () => {
+  if (started) {
+    stopGame();
+  } else {
+    startGame();
+  }
+  started = !started;
+});
+
+function startGame() {
+  initGame();
+  showStopButton();
+  showTimerAndScore();
+  startGameTimer();
+}
+
+function stopGame() {}
+
+function showStopButton() {
+  const icon = gameBtn.querySelector(".fa-play");
+  icon.classList.add("fa-stop");
+  icon.classList.remove("fa-play");
+}
+
+function showTimerAndScore() {
+  gameTimer.style.visibility = "visible";
+  gameScore.style.visibility = "visible";
+}
+
+function startGameTimer() {
+  let remainingTimeSec = GAME_DURATION_SEC;
+  updateTimerText(remainingTimeSec);
+
+  timer = setInterval(() => {
+    if (remainingTimeSec <= 0) {
+      clearInterval(timer);
+      return;
+    }
+    updateTimerText(--remainingTimeSec);
+  }, 1000);
+}
+
+function updateTimerText(timer){
+  const minutes= Math.floor(timer/60);
+  const seconds= timer%60
+  gameTimer.innerText= `${minutes}: ${seconds}`
+
 }
 
 
-function addItem(className, count, imgPath){
-const x1= 0;
-const y1= 0;
-const x2= fieldRect.width- CARROT_SIZE;
-const y2= fieldRect.height- CARROT_SIZE;
-for(let i=0; i<count; i++){
-  const item= document.createElement('img');
-  item.setAttribute('class', className);
-  item.setAttribute('src', imgPath);
-  item.style.position= 'absolute';
-  const x= randomNumber(x1,x2);
-  const y= randomNumber(y1, y2)
-  item.style.left=`${x}px`;
-  item.style.top=`${y}px`;
-  field.appendChild(item)
-}
+
+function initGame() {
+  field.innerHTML = "";
+  gameScore.innerText = CARROT_COUNT;
+  addItem("carrot", CARROT_COUNT, "img/carrot.png");
+  addItem("bug", BUG_COUNT, "img/bug.png");
 }
 
-function randomNumber(min, max){
-  return Math.random() * (max-min)+ min
+function addItem(className, count, imgPath) {
+  const x1 = 0;
+  const y1 = 0;
+  const x2 = fieldRect.width - CARROT_SIZE;
+  const y2 = fieldRect.height - CARROT_SIZE;
+  for (let i = 0; i < count; i++) {
+    const item = document.createElement("img");
+    item.setAttribute("class", className);
+    item.setAttribute("src", imgPath);
+    item.style.position = "absolute";
+    const x = randomNumber(x1, x2);
+    const y = randomNumber(y1, y2);
+    item.style.left = `${x}px`;
+    item.style.top = `${y}px`;
+    field.appendChild(item);
+  }
 }
 
-initGame()
+function randomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+initGame();
