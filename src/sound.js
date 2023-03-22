8325bd320d61091130e30eae3140e3b41b3b7e3f
kkj2010 <kunjukim0210@gmail.com>
2023-03-22 15:04:02 -0400

    fix audio

--- src/sound.js
+++ src/sound.js
@@ -1,9 +1,9 @@
 "use strict";
-const carrotSound = new Audio("../sound/carrot_pull.mp3");
-const alertSound = new Audio("../sound/alert.wav");
-const bgSound = new Audio("../sound/bg.mp3");
-const bugSound = new Audio("../sound/bug_pull.mp3");
-const winSound = new Audio("../sound/game_win.mp3");
+const carrotSound = new Audio("./sound/carrot_pull.mp3");
+const alertSound = new Audio("./sound/alert.wav");
+const bgSound = new Audio("./sound/bg.mp3");
+const bugSound = new Audio("./sound/bug_pull.mp3");
+const winSound = new Audio("./sound/game_win.mp3");
 
 export function playCarrot() {
   playSound(carrotSound);
